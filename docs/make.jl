0a769408eeb281a1b42b0795a48e56e0d0ab0648
adigitoleo <adigitoleo@dissimulo.com>
2021-08-22 17:22:09 +1000

    Fix docs manifest and revert to single version docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,4 @@
 using Documenter
-using TypedTables
 using PlateMotionRequests
 
 # Steps to build docs manually after a new release tag:
@@ -9,22 +8,3 @@ using PlateMotionRequests
 # 4. add commit and push
 
 makedocs(modules = [PlateMotionRequests], sitename="PlateMotionRequests.jl")
-root = Documenter.Utilities.currentdir()
-target = "build"
-versions = ["stable" => "v^", "v#.#"]
-latest = "v2.0.0"  # Parse this from the latest git tag?
-HTMLWriter = Documenter.Writers.HTMLWriter
-HTMLWriter.generate_siteinfo_file(target, latest)
-mktempdir() do temp
-    entries, symlinks = HTMLWriter.expand_versions(temp, versions)
-    println(entries)
-    println(symlinks)
-    HTMLWriter.generate_version_file(joinpath(temp, "versions.js"), entries, symlinks)
-    # HTMLWriter.generate_redirect_file(joinpath(temp, "index.html"), entries)
-
-    cd(temp) do
-        for kv in symlinks
-            Documenter.rm_and_add_symlink(kv.second, kv.first)
-        end
-    end
-end
